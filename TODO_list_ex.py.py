5f7bd716f6596e0a5d624671c8dd4b6647619116
Sarah Waller <sarahwaller@kubrickgroup.com>
2025-06-04 14:31:36 +0100

    added pylint and standardized

--- TODO_list_ex.py.py
+++ TODO_list_ex.py.py
@@ -2,18 +2,14 @@
 A simple to_do application that allows users to add tasks, view them, and mark them as done.
 This application is designed to be run in a terminal or command line interface.
 """
-
-def to_do_example(list_of_commands: list=[]) -> None:
+def to_do_example(list_of_commands: list) -> None:
     """
     A to do application that captures tasks and allows them to be marked off. 
-    
     Args:
         list_of_commands (list, optional): A list of commands to be executed in sequential order. 
-        
     Returns:
         None
     """
-
     listo: list = []
     for command_1 in list_of_commands:
         print("1. Add Task")
@@ -21,9 +17,9 @@ def to_do_example(list_of_commands: list=[]) -> None:
         print("3. Mark Task as Done")
         print("4. Exit")
         print("="*50)
-        command_1 = input("Enter your choice: ")
+        command_1 = input("Enter your choice:")
         if command_1 == '1':
-            tsk = input("Enter task: ")
+            tsk = input("Enter task:")
             listo.append(tsk)
             print("-"*50)
         elif command_1 == '2':
@@ -32,8 +28,8 @@ def to_do_example(list_of_commands: list=[]) -> None:
                 print(f"{i+1}. {t}")
             print("-"*50)
         elif command_1 == '3':
-            usr_input = input("Enter task number to mark as done: ")
-            if 0 <=  int(usr_input) - 1 < len(listo): 
+            usr_input = input("Enter task number to mark as done:")
+            if 0 <=  int(usr_input) - 1 < len(listo):
                 listo.pop(int(usr_input) - 1)
                 print("Task marked as done.")
             else:
@@ -45,5 +41,4 @@ def to_do_example(list_of_commands: list=[]) -> None:
         else:
             print("Invalid choice.")
             print("-"*50)
-
     return listo
